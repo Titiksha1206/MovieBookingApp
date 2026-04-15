d8868d6708ec987daf59d7b0cd31cc3bdda70875
Titiksha1206 <titiksha1206@gmail.com>
2026-04-15 22:36:59 +0530

    Controller2

--- backend/src/main/java/com/example/backend/Controller/UserController.java
+++ backend/src/main/java/com/example/backend/Controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.backend.Controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
